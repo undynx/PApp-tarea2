33b83f24840b9e86fa156ea6c070533fbfe44781
Leovs45 <leonardovieras@gmail.com>
2023-09-19 00:57:46 -0300

    avance precario pero algo para trabajar en la clase manana

---
+++ src/main/java/servlets/ConsultaUsuario.java
@@ -0,0 +1,85 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import datatypes.DtActividad;
+import datatypes.DtClase;
+import datatypes.DtProfesor;
+import datatypes.DtSocio;
+import excepciones.UsuarioNoEsProfesorException;
+import interfaces.Fabrica;
+import interfaces.IActividadDeportiva;
+import interfaces.IUsuario;
+import interfaces.IClase;
+/**
+ * Servlet implementation class ConsultaUsuario
+ */
+@WebServlet("/ConsultaUsuario")
+public class ConsultaUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConsultaUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		Fabrica fabric = Fabrica.getInstancia();
+	    IActividadDeportiva iAD = fabric.getIActividadDeportiva();
+	    IUsuario iUs = fabric.getIUsuario();
+	    IClase iCl = fabric.getIClase();
+	 // Obtén la sesión actual o crea una nueva si no existe
+	    HttpSession session = request.getSession();
+
+	    // Obtiene el valor del atributo "nickname" de la sesión
+	    String nickname = (String) session.getAttribute("nickname");
+
+	    // Verifica si el atributo existe y no es nulo antes de usarlo
+	    try {
+	    	if (nickname != null) {
+		        // Ahora puedes usar la variable "nickname" en tu servlet
+		    	if (iUs.esSocio(nickname)) {
+			    	DtSocio dtSoc = iUs.getDtSocio(nickname);
+			    	request.setAttribute("usuario", dtSoc);
+			    	/*request.setAttribute("esSocio", true);
+			    	request.setAttribute("esProfesor", false);*/
+			    } else {
+			    	DtProfesor dtProf = iUs.getDtProfesor(nickname);
+			    	request.setAttribute("usuario", dtProf);
+			    	/*request.setAttribute("esSocio", false);
+			    	request.setAttribute("esProfesor", true);*/
+			    }
+		    } else {
+		    	throw new UsuarioNoEsProfesorException("La cagaste wawachine");
+		    }
+    	} catch (UsuarioNoEsProfesorException unp) {
+	        // Manejar la excepción aquí, por ejemplo, redirigiendo a una página de error
+	        request.getRequestDispatcher("/Error.jsp").forward(request, response);
+    	}
+	}
+
+}
--- src/main/webapp/ConsultaUsuarios.jsp
+++ src/main/webapp/ConsultaUsuarios.jsp
@@ -14,6 +14,77 @@
     <div class="container mt-4">
         <p>Consulta de Usuarios</p>
     </div>
+    
+    <div class="container-fluid">
+		<div class="row">
+			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+			    <p>DIV 1</p>
+			    <button class="btn btn-outline-success btn-lg">BotonlineaGrande</button>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+			    <p>DIV 2</p>
+			    <button class="btn btn-success btn-sm">BotonverdePekeno</button>
+			
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+			    <p>DIV 2</p>
+			    <button class="btn btn-success btn-sm">Boton<button class="btn btn-success">Boton en Boton</button></button>
+			
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+			    <p>DIV 3</p>
+			    <button class="btn btn-link">NobootonTamanormal</button>
+			</div>
+		</div>			
+		<% String nick = (String) session.getAttribute("nickname"); 
+			if (nick != null) { 
+		%>	
+		<div class="row">
+			<div class="col-sm-6">Nickname</div>
+			<div class="col-sm-6">${usuario.nickname}</div>
+			<div class="col-sm-6">Nombre</div>
+			<div class="col-sm-6">${usuario.nombre}</div>
+			<div class="col-sm-6">Apellido</div>
+			<div class="col-sm-6">${usuario.apellido}</div>
+			<div class="col-sm-6">Correo Electronico</div>
+			<div class="col-sm-6">${usuario.correoElectronico}</div>
+			<div class="col-sm-6">Fecha de Nacimiento</div>
+			<div class="col-sm-6">${usuario.fechaNacimiento}</div>
+		</div>
+
+		<div class="container-fluid" aria-labelledby="datosEspecificos">
+		
+		<% String tipo = (String) session.getAttribute("tipo");
+		
+		if (tipo == "Profesor") { %>
+		<div class="row">
+			<div class="col-sm-6">Institución Deportiva:</div>
+			<div class="col-sm-6">${usuario.institucion}</div>
+			<div class="col-sm-6">Descripcion:</div>
+			<div class="col-sm-6">${usuario.descripcionGeneral}</div>
+			<div class="col-sm-6">Biografia:</div>
+			<div class="col-sm-6">${usuario.biografia}</div>
+			<div class="col-sm-6">Sitio Web:</div>
+			<div class="col-sm-6">${usuario.sitioWeb}</div>
+			<div class="col-sm-6">Lista de clases dictadas:</div>
+			<div class="col-sm-6">lista de clases</div>
+		</div>
+
+
+		<% } else { %>
+			<div class="col-sm-6">Clases a las cuales está registrado:</div>
+			<div class="col-sm-6">Lista de clases</div>
+
+		>
+		<% } %>
+		</div>
+							
+		<% } else { %>
+		
+		<div class="display-1">HA OCURRIDO UN ERROR Y USTÉ NO DEVERÍA ESTAR BIENDO ESSTO MISHISHO</div>
+		<% } %>
+	</div>
+
 
     <!-- Incluye el pie de página desde footer.jsp -->
     <%@ include file="footer.jsp" %>
